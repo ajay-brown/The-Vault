ba1ae0672ff7433dd058f1f3e14a3dab98ff8a8a
Audrey <audreybrown94@gmail.com>
2018-01-06 16:09:44 -1000

    complete

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
 module.exports = function() {
-  
+    let myObj = {};
+ function setValue (key,value) {
+  myObj[key] = value; 
+ }
+ 
+function getValue(key) {
+  if (myObj[key]) {
+        return myObj[key];
+  } else {
+      return null;
+        }
+    }
+ 
+
+return {
+    setValue: setValue,
+    getValue: getValue,
+        }
 };
